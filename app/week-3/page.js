5cee211389bf4c92fae943e7793acca93de694b6
Karl.Wu <156622572+CarofCarl@users.noreply.github.com>
2024-10-01 13:56:36 -0600

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ import ItemList from "./item-list";
 const Page = () => {
   return (
     <main className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">购物清单</h1>
+      <h2 className="text-3xl font-bold mb-6">Shopping List</h2>
       <ItemList />
     </main>
   );
